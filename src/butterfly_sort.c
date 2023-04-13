5a1106ece6fc4e25f3f087d4b2b39d9d7ab43151
Gevorg Hovhannisyan <gehovhan@c2r3s5.42yerevan.am>
2023-04-13 14:57:51 +0400

    Update header

--- src/butterfly_sort.c
+++ src/butterfly_sort.c
@@ -1,48 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   create_butterfly.c                                 :+:      :+:    :+:   */
+/*   butterfly_sort.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gehovhan <gehovhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/10 16:34:07 by gehovhan          #+#    #+#             */
-/*   Updated: 2023/04/13 14:48:53 by gehovhan         ###   ########.fr       */
+/*   Created: 2023/04/13 14:48:33 by gehovhan          #+#    #+#             */
+/*   Updated: 2023/04/13 14:56:36 by gehovhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/*
-*
-* Create Butterfly from a stack
-* 
-*/
 void	create_butterfly(t_push_swap *node)
 {
-	int i = 0;
-	int index;
-	int len = node->a.size;
-
-	while ( i < len )
-	{
-		index = node->a.head->data.index;
-		if (index <= i)
-		{
-			push(&node->a, &node->b);
-			rotate(&node->b);
-			++i;
-		}
-		else if (index <= (i + node->ratio))
-		{
-			push (&node->a, &node->b);
-			++i;
-		}
-		else
-			rotate(&node->a);
-	}
+	
 }
-
-
-
-
-
